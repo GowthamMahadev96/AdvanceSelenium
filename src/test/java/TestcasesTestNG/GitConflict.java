110af5ee588c598ce85ba2837229975109920cf2
Admin <Admin@192.168.1.37>
2025-07-29 07:41:57 +0530

    Gitconflict1

---
+++ src/test/java/TestcasesTestNG/GitConflict.java
@@ -0,0 +1,10 @@
+package TestcasesTestNG;
+
+public class GitConflict {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Conflict1");
+	}
+
+}
